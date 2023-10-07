b97dfa7d22417ffdc76b8adbe23eed0823b631b1
Kauê Schaefer de Andrade <128431286+kauzzx@users.noreply.github.com>
2023-10-07 19:18:03 -0300

    a

--- Aula 1/src/Home.js
+++ Aula 1/src/Home.js
@@ -1,31 +1,56 @@
-import { View, Text, Button} from 'react-native-web';
+import { View, Text, Button, StyleSheet, Pressable, TouchableOpacity } from 'react-native-web';
 
-export default function({
+export default function ({
     changeScreen,
     setNextScreen
-}){
+}) {
 
-    const handleClickVelha = () =>{
+    const handleClickVelha = () => {
         changeScreen("escolherNomes");
         setNextScreen("jgVelha");
     }
-    const handleClickForca = () =>{
+    const handleClickForca = () => {
         changeScreen("escolherPalavra");
     }
-    const handleClickMemoria = () =>{
+    const handleClickMemoria = () => {
         changeScreen("escolherNomes");
         setNextScreen("jgMemoria");
     }
 
-    return(
-        <View>
+    return (
+        <View >
             <Text>
                 Escolha seu minigame:
             </Text>
+            {/* <View>
+                <TouchableOpacity onPress={handleClickVelha}>
+                    <View style={styles.botao}>
+                        <Text>
+                            Jogo da Velha
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+            </View> */}
 
-            <Button title = "Jogo da Velha" onPress = {handleClickVelha}/>
-            <Button title = "Jogo da Forca" onPress = {handleClickForca}/>
-            <Button title = "Jogo da Memória" onPress = {handleClickMemoria}/>
+
+
+
+            <Button color="#3bcbdb" title="Jogo da Velha" onPress={handleClickVelha} />
+            <Button title="Jogo da Forca" onPress={handleClickForca} />
+            <Button title="Jogo da Memória" onPress={handleClickMemoria} />
         </View>
     )
 }
+
+const styles = StyleSheet({
+
+    botao: {
+        backgroundColor: 'red',
+        width: 40,
+        height: 40,
+        margin: 2,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
