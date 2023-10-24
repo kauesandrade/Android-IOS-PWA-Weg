cdedc384fa0c9cc8ec26464db9870c771dc7dae0
Kauê Schaefer de Andrade <128431286+kauzzx@users.noreply.github.com>
2023-10-24 19:08:16 -0300

    navegacao entre as telas

--- usando-rotas/App.js
+++ usando-rotas/App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './src/HomeScreen';
+import UserData from './src/UserData';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="UserData" component={UserData} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
---
+++ usando-rotas/src/UserData/UserData.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+const UserData = () =>{
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>UserData</Text>
+        </View>
+      );
+}
+
+
+export default UserData;
